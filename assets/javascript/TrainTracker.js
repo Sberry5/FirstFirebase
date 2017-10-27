0b3bf9ff53f7edb4f77f9166b3f166257578c16f
Sberry5 <shannon.berryheller@gmail.com>
2017-10-27 14:31:49 -0500

    Cleaned up code

--- assets/javascript/TrainTracker.js
+++ assets/javascript/TrainTracker.js
@@ -9,7 +9,6 @@ $(".song").trigger('load');
       }
  };
 
-
 //Initialize Firebase
   var config = {
     apiKey: "AIzaSyDhnUigJgCZi4UaXz2dppTe6FAbzmVreGc",
@@ -24,13 +23,6 @@ $(".song").trigger('load');
     //Create a variable to reference the firebase database
     var database = firebase.database();
 
-    //Initial Values
-    // var trainName = "";
-    // var destination = "";
-    // var firstTime = "";
-    // //var frequency
-    // var frequency = parseInt();
-
     //On Submit button click run the following
     $("#add-train").on("click", function(event) {
       event.preventDefault();
@@ -38,8 +30,6 @@ $(".song").trigger('load');
     //Take user input, trim values, console.log values
       var trainName = $("#trainName-input").val().trim();
       var destination = $("#destination-input").val().trim();
-      //firstTime = moment(firstTime, "hh:mm").subtract(1, "years");
-      //cosole.log(firstTime);
       var firstTime = $("#firstTime-input").val();
       var frequency = $("#frequency-input").val().trim();
       console.log(trainName);
@@ -63,50 +53,40 @@ $(".song").trigger('load');
 
   });
 
-    //
+    //Function to calculate train times using moment.js
     database.ref().on("child_added", function(childSnapshot) {
 
     // //Store values in variables
       var trainName = childSnapshot.val().trainName;
       var destination = childSnapshot.val().destination;
       var frequency = parseInt(childSnapshot.val().frequency);
-      console.log("LOOK HERE" + frequency);
       var firstTime = childSnapshot.val().firstTime;
-      console.log("HERE" + firstTime);
-      // var tblTrainArrival = childSnapshot.val().nextTrainArrival;
-      // var tblTrainWait = childSnapshot.val().waitUntilTrain;
 
     //First train time converted
     var firstTrainTime = moment(firstTime, "hh:mm").subtract(1, "years");
-    console.log(firstTrainTime);
+    //console.log(firstTrainTime);
 
     //Current time to use as base point to calculate time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    //console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     //Difference between the times
     var diffTime = moment().diff(moment(firstTime, "hh:mm"), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime); 
+    //console.log("DIFFERENCE IN TIME: " + diffTime); 
 
     //Time apart (remainder)
     var tRemainder = diffTime % frequency;
-    console.log(tRemainder);
+    //console.log(tRemainder);
 
     //Time until next train
     var waitUntilTrain = frequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + waitUntilTrain);
+    //console.log("MINUTES TILL TRAIN: " + waitUntilTrain);
 
     //Next train arrival time
     var nextTrainArrival = moment().add(waitUntilTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrainArrival).format("hh:mm"));   
+    //console.log("ARRIVAL TIME: " + moment(nextTrainArrival).format("hh:mm"));   
 
-    // //Store values in variables
-      // var tblTrainName = childSnapshot.val().trainName;
-      // var tblTrainDestination = childSnapshot.val().destination;
-      // var tblTrainFrequency = childSnapshot.val().frequency;
-      // var tblTrainArrival = childSnapshot.val().nextTrainArrival;
-      // var tblTrainWait = childSnapshot.val().waitUntilTrain;
-
+    //Add train and calculations to table
       $("#train-table > tbody").append("<tr><td>" + 
         trainName + "</td><td>" + 
         destination + "</td><td>" + 
@@ -115,6 +95,3 @@ $(".song").trigger('load');
         waitUntilTrain);
 
    });
-
-
-   // childSnapshot();
